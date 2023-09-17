a7a8ff43f44f269def2291db765638ca3233f042
Pigioty <Pigiotyreal@gmail.com>
2023-09-16 20:59:07 -0400

    net

--- src/main.py
+++ src/main.py
@@ -16,6 +16,10 @@ clock = pygame.time.Clock()
 
 def draw():
     screen.fill(black)
+    
+    for i in range(0, Height, 25):
+        pygame.draw.rect(screen, white, [Width / 2 - 2.5, i, 5, 10])
+    
     pygame.display.flip()
 
 while True:
